2d7e0a3571695f57b65fe48139b2c8306a00609e
Joe <joe.hasson2@gmail.com>
2023-04-21 19:37:28 +0100

    Fix test cases

--- c/rightrot.c
+++ c/rightrot.c
@@ -1,18 +1,19 @@
 #include "unity.h"
 
-unsigned rightrot(unsigned x) {
-    unsigned lsb = x & 1;
-    return (x >> 1) | (lsb << (sizeof(x)-1));
-}
+unsigned rightrot(unsigned x);
 
 /*test cases*/
+
+/*We need these functions to make unity work, even
+/ if they do nothing */
 void setUp(void) {
 }
 void tearDown(void) {
 }
 
 void test_rightrot(void) {
-    TEST_ASSERT_EQUAL(0x00000001U, rightrot(0x80000000U));
+    printf("rightrot(0x00000001U) = 0x%08x\n", rightrot(0x00000001U));
+    TEST_ASSERT_EQUAL(0x80000000U, rightrot(0x00000001U));
 }
 void test_rightrot2(void) {
     TEST_ASSERT_EQUAL(0x00000000U, rightrot(0x00000000U));
