b0b4d351847899c8ae43ad0e3dff94609c009659
Joe <joe.hasson2@gmail.com>
2023-04-22 16:40:06 +0100

    soln

--- py/super_implementation.py
+++ py/super_implementation.py
@@ -1,4 +1,22 @@
 import inspect
 
 class MySuper:
-    pass
+    def __init__(self):
+        local_vars = inspect.currentframe().f_back.f_locals
+        self.__self__, *_ = local_vars.values()
+        self.__self_class__ = type(self.__self__)
+        self.__thisclass__ = local_vars['__class__']
+
+    def __getattr__(self, a):
+        mro = self.__self_class__.__mro__
+        i = mro.index(self.__thisclass__) + 1
+
+        while i < len(mro):
+            if hasattr(mro[i], a):
+                if hasattr(val := getattr(mro[i], a), '__get__'):
+                    return val.__get__(self.__self__, self.__self_class__)
+                return val
+            i += 1
+
+        raise AttributeError()
+
