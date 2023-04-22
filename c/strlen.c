382498803e52a6399eb22d471a62387bbc73a9be
Joe <joe.hasson2@gmail.com>
2023-04-22 17:23:39 +0100

    Add tests, soln

--- c/strlen.c
+++ c/strlen.c
@@ -1,23 +1,42 @@
 #include <stdio.h>
+#include "unity.h"
 
 int my_strlen(char s[]);
 
-int main () {
-    char foo[] = "abc";
-    char bar[] = "hello";
-    char baz[] = "abcdefghijklmnopqrstuvwxyz";
+int my_strlen(char s[]){
+    int c = 0;
 
-    printf("abc is size %d\n", my_strlen(foo));
-    printf("hello is size %d\n", my_strlen(bar));
-    printf("abcdefghijklmnopqrstuvwxyzis size %d\n", my_strlen(baz));
-    return 0;
+    while (*s++)
+        c++;
+
+    return c;
+}
+
+// Test cases
+
+void setUp(void){}
+void tearDown(void){}
+
+void test1(void){
+    char foo[] = "abc";
+    TEST_ASSERT_EQUAL(3, my_strlen(foo));
+}
+
+void test2(void){
+    char foo[] =  "hello";
+    TEST_ASSERT_EQUAL(5, my_strlen(foo));
+}
+
+void test3(void){
+    char foo[] = "abcdefghijklmnopqrstuvwxyz";
+    TEST_ASSERT_EQUAL(26, my_strlen(foo));
+}
+
+int main (void) {
+    UNITY_BEGIN();
+    RUN_TEST(test1);
+    RUN_TEST(test2);
+    RUN_TEST(test3);
+    return UNITY_END();
     }
 
-int my_strlen(char s[]){
-    int i = 0;
-
-    while (s[i] != '\0')
-        i++;
-        
-    return i;
-}
