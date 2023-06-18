cc0bff7a872a1af113f22c04aba1893960db2e13
Joe <joe.hasson2@gmail.com>
2023-06-18 22:10:55 +0100

    Maybe improve card

--- ocaml/all_trees_from_preorder_list/all_trees_from_preorder_list.ml
+++ ocaml/all_trees_from_preorder_list/all_trees_from_preorder_list.ml
@@ -1,32 +1,36 @@
 type 'a tree = Lf | Br of 'a * 'a tree * 'a tree
 
-let split n lst = 
-  let rec aux i rest acc = match i, rest, acc with
-    | 0, rest, acc -> (List.rev acc, rest)
-    | i, (x::rest), acc ->  aux (i-1) rest (x::acc)
-    | _, [], _ -> failwith "n greater than list length"
-  in aux n lst []
+let rec cartprod l r = 
+    match l, r with
+    | [], _      -> []
+    | x :: xs, ys -> 
+            let rec pairx = function
+                | [] -> cartprod xs ys
+                | y :: ys -> (x,y) :: pairx ys
+            in pairx ys
 
-let rec cartprod xs ys = match xs, ys with
-    | [], _ -> []
-    | (x::xs), ys -> List.map (fun y -> (x,y)) ys @ (cartprod xs ys)
+let rec split_at n lst = 
+    match lst, n with
+    | lst, 0     -> [], lst
+    | [], _      -> failwith "out of bounds"
+    | x :: xs, n -> let left, right = split_at (n-1) xs in x :: left, right
 
 let rec all_trees = function
     | [] -> [Lf]
-    | (root::xs) -> 
-            let subtree_pairs i = 
-                let left, right = split i xs 
-                in cartprod (all_trees left) (all_trees right)
-            in
-            let all_pairs =
-                let rec buildpairs = function 
-                                    | 0 -> subtree_pairs 0 
-                                    | i -> subtree_pairs i @ buildpairs (i-1)
-                in buildpairs (List.length xs)
-            in
-            List.map (fun (t1,t2) -> Br(root,t1,t2)) all_pairs
+    | x :: xs -> 
+        let subtree_pairs i =
+            let left, right = split_at i xs
+            in cartprod (all_trees left) (all_trees right)
+        in
+        let all_pairs =
+            let rec build n =
+                if n=0 then subtree_pairs 0 
+                else subtree_pairs n @ build (n-1)
+            in build (List.length xs)
+        in List.map (fun (l,r) -> Br (x,l,r)) all_pairs
 
 
-let foo = (List.length (all_trees []))
-let bar = (List.length(all_trees [1;2;3]))
-let baz = (List.length(all_trees [1;2;3;4;5;6]))
+let foo = (List.length (all_trees [])) = 1
+let bar = (List.length(all_trees [1;2;3])) = 5
+let baz = (List.length(all_trees [1;2;3;4;5;6])) = 132
+
