25bbe8eaa4daca1df125b211b0b09ddd7b99d67f
Joe <joe.hasson2@gmail.com>
2023-07-15 18:44:46 +0100

    Update card

--- ocaml/nqueens/nqueens.ml
+++ ocaml/nqueens/nqueens.ml
@@ -1,43 +1,24 @@
-type 'a seq = Nil | Cons of 'a * (unit -> 'a seq)
-
 let dfs nexts init =
-    let rec aux = function
-        | [] -> Nil
-        | x::stack -> Cons (x, fun () -> aux (nexts x @ stack))
+    let rec aux stack = match stack with
+    | [] -> Seq.empty
+    | x :: xs -> fun () -> Seq.Cons (x, aux (nexts x @ xs))
     in aux [init]
 
-let rec seq_filter f = function
-    | Nil -> Nil
-    | Cons (a, fsq) -> 
-            let sq = fsq () in 
-            if f a 
-            then Cons (a, fun () -> seq_filter f sq) 
-            else seq_filter f sq
-
-let nqueen_solutions n = 
-    let next_queens board =
-        let is_safe new_q = 
-            let no_diags new_q =
-                let rec aux i qs = 
-                    match qs with
-                    | [] -> true
-                    | q::qs' -> (Int.abs(new_q - q) <> i) && aux (i+1) qs'
-                in aux 1 board
-            in
-            no_diags new_q && not (List.mem new_q board)
-        in
-        let safe_qs = List.filter is_safe (List.init n (fun n -> n + 1)) in 
-        List.map (fun q -> q :: board) safe_qs 
+let next_safe_boards size oldqs =
+    let no_diags q qs =
+        let rec aux i qs = match qs with
+        | [] -> true
+        | x :: xs -> Int.abs (q - x) <> i && aux (i+1) xs
+        in aux 1 qs
     in
-    seq_filter (fun qs -> List.length qs = n) (dfs next_queens [])
+    let positions = List.init size (fun x -> x) 
+    in
+    let safe_positions = 
+        List.filter (fun p -> no_diags p oldqs && not (List.mem p oldqs)) positions
+    in List.map (fun p -> p :: oldqs) safe_positions
 
-(* Test cases *)
-
-let rec seq_len = function
-    | Nil -> 0
-    | Cons (_, fsq) -> 1 + seq_len (fsq())
-
-let sol8 = seq_len(nqueen_solutions 8)
-let sol3 = seq_len(nqueen_solutions 4)
-let sol3 = seq_len(nqueen_solutions 3)
+let solve n =
+    let is_complete board = List.length board = n  in
+    let safe_boards = dfs (next_safe_boards n) [] in
+    Seq.filter is_complete safe_boards
 
--- ocaml/nqueens/test.ml
+++ ocaml/nqueens/test.ml
@@ -1,17 +1,16 @@
 open OUnit2
 open Nqueens
 
-let count_solns dimension = 
-    let rec aux acc = function
-        | Nil -> acc
-        | Cons(_,xf) -> aux (1+acc) (xf ())
-    in aux 0 (nqueens dimension)
+let count_solns dimension =  
+    Seq.length (solve dimension)
 
+let maketest expected actual =
+    assert_equal expected actual ~printer:Int.to_string
 
 let tests = "test suite for nqueens" >:::  [
-    "3 queens" >:: (fun _ -> assert_equal 0 (count_solns 3));
-    "4 queens" >:: (fun _ -> assert_equal 2 (count_solns 4));
-    "8 queens" >:: (fun _ -> assert_equal 92 (count_solns 8));
+    "3 queens" >:: (fun _ -> maketest 0 (count_solns 3));
+    "4 queens" >:: (fun _ -> maketest 2 (count_solns 4));
+    "8 queens" >:: (fun _ -> maketest 92 (count_solns 8));
 ]
 
 let _ = run_test_tt_main tests
