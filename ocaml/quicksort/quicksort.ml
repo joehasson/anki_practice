5ff81df215a01283065db64070c3b08bc1bafcdc
Joe <joe.hasson2@gmail.com>
2023-08-02 12:18:56 +0100

    Delete bad version of card

--- ocaml/quicksort/quicksort.ml
+++
@@ -1,8 +0,0 @@
-let quicksort list = 
-    let rec aux list sorted = match list with
-            | [] -> sorted
-            | (x::xs) ->
-                    let left, right = List.partition (fun y -> y < x) xs
-                    in aux left (x :: aux right sorted)
-    in aux list []
-
--- ocaml/quicksort/test.ml
+++
@@ -1,3 +0,0 @@
-open Quicksort
-open Ounit2
-
