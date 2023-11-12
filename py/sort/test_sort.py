d73046ecd698945fa46cc0d9c9a72042a2cd0dc3
Joe <joe.hasson2@gmail.com>
2023-11-12 14:26:05 +0000

    Randomised testing

--- py/sort/test_sort.py
+++ py/sort/test_sort.py
@@ -1,23 +1,31 @@
+import itertools
+import random
 import pytest
 from enum import Enum
 
 import bottom_up_mergesort
+import heap_sort
 import insertion_sort
 import quicksort
 import selection_sort
+import top_down_mergesort
 
 class SortType(Enum):
     IN_PLACE = 0
     PURE = 1
 
+params = (
+    #pytest.param(bottom_up_mergesort.sort, SortType.PURE, id="Bottom up mergesort"),
+    pytest.param(heap_sort.sort, SortType.IN_PLACE, id="Heap sort"),
+    pytest.param(insertion_sort.sort, SortType.IN_PLACE, id="Insertion sort"),
+    pytest.param(quicksort.sort, SortType.IN_PLACE, id="Quicksort"),
+    pytest.param(selection_sort.sort, SortType.IN_PLACE, id="Selection sort"),
+    #pytest.param(top_down_mergesort.sort, SortType.PURE, id="Top down mergesort"),
+)
+
 @pytest.mark.parametrize(
         ('sort', 'typ'),
-        (
-            pytest.param(bottom_up_mergesort.sort, SortType.PURE, id="Bottom up mergesort"),
-            pytest.param(insertion_sort.sort, SortType.IN_PLACE, id="Insertion sort"),
-            pytest.param(quicksort.sort, SortType.IN_PLACE, id="Quicksort"),
-            pytest.param(selection_sort.sort, SortType.IN_PLACE, id="Selection sort"),
-        )
+        params
 )
 @pytest.mark.parametrize(
         ('input', 'expected'),
@@ -34,3 +42,22 @@ def test_sort(sort, typ, input, expected) -> None:
         assert input == expected
     elif typ == SortType.PURE:
         assert sort(input) == expected
+
+
+@pytest.mark.parametrize(
+        ('sort', 'typ'),
+        params
+)
+def test_sort_randoms(sort, typ) -> None:
+    for _ in range(10):
+        size = random.randint(1, 1000)
+        random_lst = [random.randint(1, 100000) for _ in range(size)]
+        if typ == SortType.PURE:
+            assert is_sorted(sort(random_lst))
+        elif typ == SortType.IN_PLACE:
+            sort(random_lst)
+            assert is_sorted(random_lst)
+
+
+def is_sorted(lst: list) -> bool:
+    return all(a <= b for a, b in itertools.pairwise(lst))
